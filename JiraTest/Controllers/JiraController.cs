1aaaf7c45f79961e536a1179a5282f8b2eea982e
Nhân Đỗ Trí <nhandtn2001@gmail.com>
2022-03-19 21:33:14 +0700

    Hello

---
+++ JiraTest/Controllers/JiraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JiraTest.Controllers
+{
+    public class JiraController : Controller
+    {
+        // GET: Jira
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
